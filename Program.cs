d4bc38867d8eaffd3b89b4b1786078e850187ee4
Mohd Shakaib Ghazi <mohd.shakaibghazi@mphasis.com>
2023-07-12 15:01:09 +0530

    First

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string value ="865";
+                int numeric;
+                bool isTrue = int.TryParse(value, out numeric);
+                if (isTrue)
+                {
+                    Console.WriteLine("The Integer value is " + numeric);
+                    Console.ReadKey();
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();  
+            }
+        }
+    }
+}
